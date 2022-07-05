185fa250e2cb0d9c9f949e23fe4be9e9da719afa
Error <88781785+TheSadError@users.noreply.github.com>
2022-07-05 15:03:50 +0400

    Update speechtotext.py

--- speechtotext.py
+++ speechtotext.py
@@ -2,6 +2,7 @@ from gtts import gTTS
 import os
 
 text = "lol"
-speech = gTTS(text=text, lang='english', slow=False)
+speech = gTTS(text=text, lang='en', slow=False)
 speech.save("voice.mp3")
 os.system("voice.mp3")
+# you you must put en not english because grrs library dont has english
